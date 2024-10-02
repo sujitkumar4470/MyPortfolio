9d0a1f6dbdbaee822e09833fc724eee06366e8cc
sujitkumar4470 <jhzb82@gmail.com>
2024-10-02 11:53:28 +0530

    first

---
+++ port.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+$(document).scroll(function(){
+    $(".navDetail").css("color","black");
+    $(".change").css("color","black");
+    $(".navDetail").css("background","#E1D9D1");
+    $(".fa-phone").css("background","#14b789");
+    $(".fa-phone").css("border-color","#14b789");
+    $(".fa-phone").css("color","white");
+    $(".navCont").css("border-color","#14b789")
+    $(".navCont").css("color","#14b789")
+    
+
+})
+})
